67560ff6fb098cdaac8d9da5570948243b9c657b
Peter Liu <pliu7@binghamton.edu>
2013-07-04 05:23:02 -0400

    finished last stages of treehouse

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = 'd5cafc7e77af93fc8f2cdf2222f4e5859c7f7a3b8d66ec5ee2a47d7272de87f5c47744fb0814f041f298ca60a8ce55d7ef9b3e3e7d6836f640281f02592d3e60'
---
+++ test/unit/user_test.rb
@@ -0,0 +1,48 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  test "a user should enter a first name" do
+    user = User.new
+    assert !user.save
+    assert !user.errors[:first_name].empty?
+  end
+
+  test "a user should enter a last name" do
+    user = User.new
+    assert !user.save
+    assert !user.errors[:last_name].empty?
+  end
+
+  test "a user should enter a profile name" do
+    user = User.new
+    assert !user.save
+    assert !user.errors[:profile_name].empty?
+  end
+
+  test "a user should have a unique profile name" do
+    user = User.new
+    user.profile_name = users(:jason).profile_name
+    
+    assert !user.save
+    assert !user.errors[:profile_name].empty?
+  end
+
+  test "a user should have a profile name without spaces" do
+    user = User.new
+    user = User.new(first_name: 'Peter', last_name: 'Liu', email: 'pliu131@gmail.com')
+    user.password = user.password_confirmation = 'asdfasdf'
+    user.profile_name = "My Profile With Spaces"
+
+    assert !user.save
+    assert !user.errors[:profile_name].empty?
+    assert user.errors[:profile_name].include?("must be formatted correctly.")
+  end
+
+  test "a user can have a correctly formatted profile name" do
+    user = User.new(first_name: 'Peter', last_name: 'Liu', email: 'pliu131@gmail.com')
+    user.password = user.password_confirmation = 'asdfasdf'
+    user.profile_name = 'pliu131'
+    assert user.valid?
+  end
+
+end
